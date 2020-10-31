250cf42f262d35e2bf14de9f23349633e63d630e
Gianpiero Addis <gianpiero.addis@gmail.com>
2020-10-31 14:54:05 +0100

    Use subject

--- spec/support/shared_examples/client.rb
+++ spec/support/shared_examples/client.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'client' do
-  let(:browser) { described_class.new }
+  subject(:browser) { described_class.new }
 
   it 'has a user agent' do
-    expect(described_class.new.user_agent).to be_a String
+    expect(browser.user_agent).to be_a String
   end
 
   describe '#user_agent' do
     it 'returns a random different user agent with different instances' do
-      expect(described_class.new.user_agent).not_to eq described_class.new.user_agent
+      expect(browser.user_agent).not_to eq described_class.new.user_agent
     end
 
     it 'persists the user agent in the instance' do
